9e827742443444fe84c95c4503ac5fd653b94c16
patil <tutorpatil@gmail.com>
2017-11-26 09:23:34 +0530

    changing the implicittimeout to 10 -- Patil

--- src/test/java/com/selenium/base/BaseClass.java
+++ src/test/java/com/selenium/base/BaseClass.java
@@ -135,7 +135,7 @@ public class BaseClass implements ITestListener{
 		}
 		
 			
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		
 		driver.get(url);
 		
 			
